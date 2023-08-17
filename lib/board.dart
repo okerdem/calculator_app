f5c8f906c9b171936ec7cf0ec06fe96b37b380e4
odman <okerdem.13@gmail.com>
2023-08-17 22:31:10 +0300

    - - - - - - -
    feat: add buttons
    
    - - - - - - -

--- lib/board.dart
+++ lib/board.dart
@@ -12,29 +12,53 @@ class _BoardState extends State<Board> {
   double _result = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: Colors.grey,
-      body: Column(children: [
-        const Padding(
-          padding: EdgeInsets.symmetric(vertical: 30.0, horizontal: 16.0),
-          child: TextField(
-            decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: ":3",
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 30.0, horizontal: 16.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: ":3",
+              ),
             ),
           ),
-        ),
-        Row(
-          children: [
-            ElevatedButton(onPressed: null, child: Text("1")),
-            ElevatedButton(onPressed: null, child: Text("2")),
-            ElevatedButton(onPressed: null, child: Text("3")),
-            Container(
-              child: IconButton(onPressed: null, icon: Icon(Icons.add)),
-            )
-          ],
-        )
-      ]),
+          Row(
+            children: [
+              ElevatedButton(onPressed: null, child: Text("1")),
+              ElevatedButton(onPressed: null, child: Text("2")),
+              ElevatedButton(onPressed: null, child: Text("3")),
+              ElevatedButton(onPressed: null, child: Text("+")),
+            ],
+          ),
+          Row(
+            children: [
+              ElevatedButton(onPressed: null, child: Text("4")),
+              ElevatedButton(onPressed: null, child: Text("5")),
+              ElevatedButton(onPressed: null, child: Text("6")),
+              ElevatedButton(onPressed: null, child: Text("-")),
+            ],
+          ),
+          Row(
+            children: [
+              ElevatedButton(onPressed: null, child: Text("7")),
+              ElevatedButton(onPressed: null, child: Text("8")),
+              ElevatedButton(onPressed: null, child: Text("9")),
+              ElevatedButton(onPressed: null, child: Text("x")),
+            ],
+          ),
+          Row(
+            children: [
+              Spacer(),
+              ElevatedButton(onPressed: null, child: Text("0")),
+              Spacer(),
+              ElevatedButton(onPressed: null, child: Text("/")),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
