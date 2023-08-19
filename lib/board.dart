c532d697a777f7438e34b633e546c1e8843f3e47
odman <okerdem.13@gmail.com>
2023-08-19 22:15:43 +0300

    - - - - - - -
    style: change the design of the operator buttons
    
    - - - - - - -

--- lib/board.dart
+++ lib/board.dart
@@ -10,53 +10,171 @@ class Board extends StatefulWidget {
 
 class _BoardState extends State<Board> {
   double _result = 0;
+  String _textField = "";
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.grey,
+    return Scaffold(
+      backgroundColor: Colors.orange.shade50,
       body: Column(
         children: [
+          Spacer(),
           Padding(
             padding: EdgeInsets.symmetric(vertical: 30.0, horizontal: 16.0),
             child: TextField(
+              textAlign: TextAlign.center,
               decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: ":3",
-              ),
+                  border: OutlineInputBorder(),
+                  hintText: ":3",
+                  labelText: _textField,
+                  labelStyle: TextStyle(
+                    fontSize: 25,
+                  )),
             ),
           ),
           Row(
             children: [
-              ElevatedButton(onPressed: null, child: Text("1")),
-              ElevatedButton(onPressed: null, child: Text("2")),
-              ElevatedButton(onPressed: null, child: Text("3")),
-              ElevatedButton(onPressed: null, child: Text("+")),
-            ],
-          ),
-          Row(
-            children: [
-              ElevatedButton(onPressed: null, child: Text("4")),
-              ElevatedButton(onPressed: null, child: Text("5")),
-              ElevatedButton(onPressed: null, child: Text("6")),
-              ElevatedButton(onPressed: null, child: Text("-")),
-            ],
-          ),
-          Row(
-            children: [
-              ElevatedButton(onPressed: null, child: Text("7")),
-              ElevatedButton(onPressed: null, child: Text("8")),
-              ElevatedButton(onPressed: null, child: Text("9")),
-              ElevatedButton(onPressed: null, child: Text("x")),
+              Spacer(),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _textField += "1";
+                    });
+                  },
+                  child: Text("1")),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _textField += "2";
+                    });
+                  },
+                  child: Text("2")),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _textField += "3";
+                    });
+                  },
+                  child: Text("3")),
+              ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.amber.shade100,
+                ),
+                child: Text(
+                  "+",
+                  style: TextStyle(
+                    color: Colors.orange,
+                  ),
+                ),
+              ),
+              Spacer(),
             ],
           ),
           Row(
             children: [
               Spacer(),
-              ElevatedButton(onPressed: null, child: Text("0")),
+              Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: ElevatedButton(onPressed: null, child: Text("4")),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: ElevatedButton(onPressed: null, child: Text("5")),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: ElevatedButton(onPressed: null, child: Text("6")),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amber.shade100,
+                    ),
+                    child: Text(
+                      "-",
+                      style: TextStyle(
+                        color: Colors.orange,
+                      ),
+                    )),
+              ),
               Spacer(),
-              ElevatedButton(onPressed: null, child: Text("/")),
             ],
-          )
+          ),
+          Row(
+            children: [
+              Spacer(),
+              Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: ElevatedButton(onPressed: null, child: Text("7")),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: ElevatedButton(onPressed: null, child: Text("8")),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: ElevatedButton(onPressed: null, child: Text("9")),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amber.shade100,
+                    ),
+                    child: Text(
+                      "x",
+                      style: TextStyle(
+                        color: Colors.orange,
+                      ),
+                    )),
+              ),
+              Spacer(),
+            ],
+          ),
+          Row(
+            children: [
+              Spacer(),
+              Spacer(),
+              Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: ElevatedButton(onPressed: null, child: Text("0")),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.amber.shade100,
+                  ),
+                  child: Text(
+                    "=",
+                    style: TextStyle(
+                      color: Colors.orange,
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 8),
+                child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.amber.shade100,
+                    ),
+                    child: Text(
+                      "/",
+                      style: TextStyle(
+                        color: Colors.orange,
+                      ),
+                    )),
+              ),
+              Spacer(),
+            ],
+          ),
+          Spacer(),
         ],
       ),
     );
